d666a3736011b8aca7c8cd65610d8902f3730796
yodevhail <yodevhail@gmail.com>
2018-10-03 15:09:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Today is October 3, 2018. I am Yonas and alive!!");
+	// write your code here
+    }
+}
